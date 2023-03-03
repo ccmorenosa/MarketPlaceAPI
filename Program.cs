ad7293508f4f12746b097cadf6d79df5b938f734
Catalina Sarria <cimorenos@unal.edu.co>
2023-03-02 20:12:41 -0500

    Add the database to the program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MarketPlaceAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<MarketContext>(
+    opt => opt.UseInMemoryDatabase("Market")
+);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
